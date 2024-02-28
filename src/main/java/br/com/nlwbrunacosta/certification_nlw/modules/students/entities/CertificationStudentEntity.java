9f55340cf977a33d8603387509cb879626a9a444
brunapcosta <brunaa.jornalismo@gmail.com>
2024-02-28 15:30:31 -0300

    config

--- src/main/java/br/com/nlwbrunacosta/certification_nlw/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/br/com/nlwbrunacosta/certification_nlw/modules/students/entities/CertificationStudentEntity.java
@@ -6,6 +6,9 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -43,8 +46,9 @@ public class CertificationStudentEntity {
     @JoinColumn(name = "student_id", insertable = false, updatable = false)
     private StudentEntity studentEntity;
     
-    @OneToMany
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
+    @JsonManagedReference
     private List<AnswersCertificationsEntity> answersCertificationsEntities;
 
     @CreationTimestamp
--- src/main/java/br/com/nlwbrunacosta/certification_nlw/modules/students/useCases/StudentCertificationAnswersUseCase.java
+++ src/main/java/br/com/nlwbrunacosta/certification_nlw/modules/students/useCases/StudentCertificationAnswersUseCase.java
@@ -3,6 +3,7 @@ package br.com.nlwbrunacosta.certification_nlw.modules.students.useCases;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,9 @@ public class StudentCertificationAnswersUseCase {
     public CertificationStudentEntity execute(StudentCertificationAnswerDTO dto) {
 
         List<QuestionEntity> questionsEntity = questionRepository.findByTechnology(dto.getTechnology());
+        List<AnswersCertificationsEntity> answersCertifications = new ArrayList<>();
+
+        AtomicInteger correctAnswers = new AtomicInteger(0);
 
         dto.getQuestionsAnswers()
         .stream().forEach(questionAnswer -> {
@@ -41,9 +45,18 @@ public class StudentCertificationAnswersUseCase {
 
             if (findCorrectAlternative.getId().equals(questionAnswer.getAlternativeID())){
                 questionAnswer.setCorrect(true);
+                correctAnswers.incrementAndGet();
             } else{
                 questionAnswer.setCorrect(false);
             }
+
+            var answerrsCertificationsEntity = AnswersCertificationsEntity.builder()
+            .answerID(questionAnswer.getAlternativeID())
+            .questionID(questionAnswer.getQuestionID())
+            .isCorrect(questionAnswer.isCorrect()).build();
+
+            answersCertifications.add(answerrsCertificationsEntity);
+
         });
 
         var student = studentRepository.findByEmail(dto.getEmail());
@@ -56,15 +69,22 @@ public class StudentCertificationAnswersUseCase {
             studentID = student.get().getId();
         }
 
-        List<AnswersCertificationsEntity> answersCertifications = new ArrayList<>();
-
         CertificationStudentEntity certificationStudentEntity = CertificationStudentEntity.builder()
             .technology(dto.getTechnology())
             .studentID(studentID)
-            .answersCertificationsEntities(answersCertifications)
+            .grade(correctAnswers.get())
             .build();
 
         var certificationStudentCreated =  certificationStudentRepository.save(certificationStudentEntity);
+
+        answersCertifications.stream().forEach(answersCertification -> {
+            answersCertification.setCertificationID(certificationStudentEntity.getId());
+            answersCertification.setCertificationStudentEntity(certificationStudentEntity);
+        });
+
+        certificationStudentEntity.setAnswersCertificationsEntities(answersCertifications);
+
+        certificationStudentRepository.save(certificationStudentEntity);
 
         return certificationStudentCreated;
     
